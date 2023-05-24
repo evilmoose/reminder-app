d1549c071b1d60b35fd7735a08eaae5f649c744f
Evilmoose <ronnielopez@gmail.com>
2023-05-24 12:44:46 -0700

    List and Person Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import data from './data.js';
+import List from './List.jsx';
 
 const App = () => {
-  const [poeple, setPeople] = useState(data);
-  console.log(poeple);
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List poeple={people} />
+      </section>
+    </main>
+  );
 };
 export default App;
