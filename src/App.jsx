499f3123ef425dfc6db9fd0cb0aeffe5186cc0cb
Evilmoose <ronnielopez@gmail.com>
2023-05-24 12:35:43 -0700

    Setup State Variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import data from './data.js';
 
 const App = () => {
+  const [poeple, setPeople] = useState(data);
+  console.log(poeple);
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;
